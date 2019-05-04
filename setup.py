9c0c4f9f4a83c5d82940bfb804b172f3fcdad198
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-05-05 04:05:45 +0530

    data-added

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jimner",
-    version="1.1",
+    version="1.2.3",
     author="Jimut Bahan Pal",
     author_email="jimutbahanpal@yahoo.com",
     description="A banner generating library for UNIX, LINUX, MAC-OS and WINDOWS",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Jimut123/jimner",
-    install_requires=['json'],
+    #install_requires=['json==2.0.9'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    include_package_data=True,
+    package_data={u'jimner':[u'fonts/*.json']},
 )
